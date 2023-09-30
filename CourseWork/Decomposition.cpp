e6687719a202ff501492590b8b107f194ddd2b09
Vsevolod Bietepage <v.bietepage@gmail.com>
2023-09-30 22:00:48 +0300

    remove some pointers and comments

--- CourseWork/Decomposition.cpp
+++ CourseWork/Decomposition.cpp
@@ -4,21 +4,12 @@ Decomposition::Decomposition(Grid originalGrid, Grid enlargedGrid, int rank, int
 	setOriginalFlow(originalGrid);
 	FindEmbeddingAndExtensionMatrix(originalGrid, enlargedGrid);
 	FindMainFlow(rank, numtasks);
-	FindWaveletFlow();
+	FindWaveletFlow(rank, numtasks);
 }
-
-//Decomposition::~Decomposition()
-//{
-//	mOriginalFlow = NULL;
-//	mMainFlow = NULL;
-//	mWaveletFlow = NULL;
-//	mEmbeddingMatrix = NULL;
-//	mExtensionMatrix = NULL;
-//}
 
 void Decomposition::setOriginalFlow(Grid& original) {
 	auto flow = original.getValues();
-	*mOriginalFlow = Map<VectorXd, Unaligned>(flow.data(), flow.size());
+	mOriginalFlow = Map<VectorXd, Unaligned>(flow.data(), flow.size());
 }
 
 void Decomposition::FindEmbeddingAndExtensionMatrix(Grid& original, Grid& enlarged) {
@@ -26,63 +17,42 @@ void Decomposition::FindEmbeddingAndExtensionMatrix(Grid& original, Grid& enlarg
 	auto original_flow = original.getValues();
 	const int columns_count = original_flow.rows() - 1;
 	const int rows_count = enlarged_flow.rows() - 1;
-	SparseMatrix<int>* embedding_matrix = new SparseMatrix<int>(rows_count, columns_count);
-	SparseMatrix<int>* extension_matrix = new SparseMatrix<int>(rows_count, columns_count);
+	SparseMatrix<int> embedding_matrix(rows_count, columns_count);
+	SparseMatrix<int> extension_matrix(rows_count, columns_count);
 	int row = 0;
 
-	embedding_matrix->insert(0, 0) = 1;
-	extension_matrix->insert(0, 0) = 1;
+	embedding_matrix.insert(0, 0) = 1;
+	extension_matrix.insert(0, 0) = 1;
 	for (int i = 1; i < columns_count; i++)
 	{
 		if (enlarged_flow(row + 1) == original_flow(i)) {
 			row++;
-			extension_matrix->insert(row, i) = 1;
+			extension_matrix.insert(row, i) = 1;
 		}
-		embedding_matrix->insert(row, i) = 1;
+		embedding_matrix.insert(row, i) = 1;
 	}
-	*mEmbeddingMatrix = *embedding_matrix;
-	*mExtensionMatrix = *extension_matrix;
-	delete embedding_matrix;
-	delete extension_matrix;
+	mEmbeddingMatrix = embedding_matrix;
+	mExtensionMatrix = extension_matrix;
 }
 
 void Decomposition::FindMainFlow(int rank, int numtasks) {
-	//auto extension_matrix = getExtensionMatrix();
-	//auto original_flow = getOriginalFlow();
-	//VectorXd main(extension_matrix.rows());
-	//for (int i = 0; i < extension_matrix.rows(); ++i) {
-	//	for (int j = 0; j < extension_matrix.cols(); ++j) {
-	//		if (extension_matrix.coeff(i, j) == 1) {
-	//			main(i) = original_flow(j);
-	//			break;
-	//		}
-	//	}
-	//}
-	//*mMainFlow = main;
 	MpiHelper mpi = MpiHelper();
 	VectorXd original = getOriginalFlow();
 	SparseMatrix<int> extension = getExtensionMatrix();
 	auto vector_flow = mpi.MpiMainFlow(original, extension, rank, numtasks);
-	//delete &extension;
-	//*mMainFlow = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(vector_flow.data(), vector_flow.size());
-	*mMainFlow = vector_flow;
+	mMainFlow = vector_flow;
 
 }
 
-void Decomposition::FindWaveletFlow() {
+void Decomposition::FindWaveletFlow(int rank, int numtasks) {
+	MpiHelper mpi = MpiHelper();
 	auto embedding_matrix = getEmbeddingMatrix();
 	auto extension_matrix = getExtensionMatrix();
 	auto original_flow = getOriginalFlow();
-	VectorXd wavelet = VectorXd::Zero(extension_matrix.cols());
-	SparseMatrix<int>* identity = new SparseMatrix<int>(extension_matrix.cols(), extension_matrix.cols());
-	identity->setIdentity();
-	SparseMatrix<int> wavelet_matrix = *identity - embedding_matrix.transpose() * extension_matrix;
-	for (int i = 0; i < wavelet_matrix.rows(); ++i) {
-		for (int j = 0; j < wavelet_matrix.cols(); ++j) {
-			wavelet(i) += original_flow(j) * wavelet_matrix.coeff(i,j);
-		}
-	}
-	*mWaveletFlow = wavelet;
-	delete identity;
+	SparseMatrix<int> identity(extension_matrix.cols(), extension_matrix.cols());
+	identity.setIdentity();
+	SparseMatrix<int> wavelet_matrix = identity - embedding_matrix.transpose() * extension_matrix;
+	auto vector_flow = mpi.MpiWaveletFlow(original_flow, wavelet_matrix, rank, numtasks);
+	mWaveletFlow = vector_flow;
 }
 
--- CourseWork/Decomposition.h
+++ CourseWork/Decomposition.h
@@ -9,27 +9,26 @@ using namespace Eigen;
 class Decomposition
 {
 private:
-	VectorXd* mOriginalFlow = new VectorXd;
-	VectorXd* mMainFlow = new VectorXd;
-	VectorXd* mWaveletFlow = new VectorXd;
-	SparseMatrix<int>* mEmbeddingMatrix = new SparseMatrix<int>;
-	SparseMatrix<int>* mExtensionMatrix = new SparseMatrix<int>;
+	VectorXd mOriginalFlow;
+	VectorXd mMainFlow;
+	VectorXd mWaveletFlow;
+	SparseMatrix<int> mEmbeddingMatrix;
+	SparseMatrix<int> mExtensionMatrix;
 	void setOriginalFlow(Grid& original);	
 	void FindEmbeddingAndExtensionMatrix(Grid& original, Grid& enlarged);
 	void FindMainFlow(int rank, int numtasks);
-	void FindWaveletFlow();
+	void FindWaveletFlow(int rank, int numtasks);
 
 
 public:
 	Decomposition(Grid originalGrid, Grid enlargedGrid, int rank, int numtasks);
 
-	VectorXd getOriginalFlow() { return *mOriginalFlow; }
-	VectorXd getMainFlow() { return *mMainFlow; }
-	VectorXd getWaveletFlow() { return *mWaveletFlow; }
-	SparseMatrix<int> getEmbeddingMatrix() { return *mEmbeddingMatrix; }
-	SparseMatrix<int> getExtensionMatrix() { return *mExtensionMatrix; }
+	VectorXd getOriginalFlow() { return mOriginalFlow; }
+	VectorXd getMainFlow() { return mMainFlow; }
+	VectorXd getWaveletFlow() { return mWaveletFlow; }
+	SparseMatrix<int> getEmbeddingMatrix() { return mEmbeddingMatrix; }
+	SparseMatrix<int> getExtensionMatrix() { return mExtensionMatrix; }
 
 	Decomposition() = default;
-	//~Decomposition();
 };
 
