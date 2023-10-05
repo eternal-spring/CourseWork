144c7f8e7738bdc3900031f0e4056eb865cb0ea0
Vsevolod Bietepage <v.bietepage@gmail.com>
2023-10-05 23:36:50 +0300

    remove MpiHelper::EigenToStd and add space

--- CourseWork/MpiHelper.h
+++ CourseWork/MpiHelper.h
@@ -1,15 +1,14 @@
 #pragma once
 #include <vector>
 #include <Eigen/Dense>
-#include<Eigen/Sparse>
+#include <Eigen/Sparse>
 #include <mpi.h>
 
 class MpiHelper
 {
 public:
-	std::vector<double> EigenToStd(Eigen::VectorXd vector);
-	std::vector<double> MainFlowForRank(Eigen::VectorXd original, Eigen::SparseMatrix<int> extension, int rank, int numranks);
-	Eigen::VectorXd MpiWaveletFlow(Eigen::VectorXd original, Eigen::SparseMatrix<int> wavelet, int rank, int numranks);
+	std::vector<double> MainFlowForRank(Eigen::VectorXd& original, Eigen::SparseMatrix<int>& extension, int rank, int numranks);
+	Eigen::VectorXd MpiWaveletFlow(Eigen::VectorXd& original, Eigen::SparseMatrix<int>& wavelet, int rank, int numranks);
 	Eigen::VectorXd MpiMainFlow(Eigen::VectorXd& original, Eigen::SparseMatrix<int>& extension, int rank, int numtasks);
 	std::vector<double> WaveletFlowForRank(Eigen::VectorXd& original, Eigen::SparseMatrix<int>& wavelet, int rank, int numtasks);
 	MpiHelper();
