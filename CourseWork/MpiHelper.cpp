316006903e15c9af0e89dc69c82df60766d0fc33
Vsevolod Bietepage <v.bietepage@gmail.com>
2023-10-06 22:46:39 +0300

    fix flow for rank calculation and variables naming

--- CourseWork/MpiHelper.cpp
+++ CourseWork/MpiHelper.cpp
@@ -4,8 +4,10 @@ Eigen::VectorXd MpiHelper::MpiMainFlow(Eigen::VectorXd& original, Eigen::SparseM
 	int size = extension.rows();
 	std::vector<double> local_main = MainFlowForRank(original, extension, rank, numtasks);
 	std::vector<double> total_main(size);
-	MPI_Reduce(&local_main[0], &total_main[0], size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	return Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(&total_main[0], size);
+	MPI_Reduce(local_main.data(), total_main.data(), size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Bcast(total_main.data(), size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	return Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(total_main.data(), size);
 }
 
 std::vector<double> MpiHelper::WaveletFlowForRank(Eigen::VectorXd& original, Eigen::SparseMatrix<int>& wavelet, int rank, int ranks_num)
@@ -15,8 +17,9 @@ std::vector<double> MpiHelper::WaveletFlowForRank(Eigen::VectorXd& original, Eig
 	int size = wavelet_matrix.rows();
 	std::vector<double> local_wavelet(size, 0.0);
 	auto partition = size / ranks_num;
-	for (int i = partition * rank; i < partition * rank + partition; ++i) {
-		if (i == size) break;
+	int lower_bound = partition * rank;
+	int upper_bound = rank == ranks_num - 1 ? size : lower_bound + partition;
+	for (int i = lower_bound; i < upper_bound; ++i) {
 		for (int j = 0; j < wavelet_matrix.cols(); ++j) {
 			local_wavelet.at(i) += original_flow(j) * wavelet_matrix.coeff(i, j);
 		}
@@ -34,8 +37,9 @@ std::vector<double> MpiHelper::MainFlowForRank(Eigen::VectorXd& original, Eigen:
 	int size = extension_matrix.rows();
 	std::vector<double> local_main(size, 0.0);
 	auto partition = size / ranks_num;
-	for (int i = partition * rank; i < partition * rank + partition; ++i) {
-		if (i == size) break;
+	int lower_bound = partition * rank;
+	int upper_bound = rank == ranks_num - 1 ? size : lower_bound + partition;
+	for (int i = lower_bound; i < upper_bound; ++i) {
 		for (int j = 0; j < extension_matrix.cols(); ++j) {
 			if (extension_matrix.coeff(i, j) == 1) {
 				local_main.at(i) = original_flow(j);
@@ -49,8 +53,10 @@ std::vector<double> MpiHelper::MainFlowForRank(Eigen::VectorXd& original, Eigen:
 Eigen::VectorXd MpiHelper::MpiWaveletFlow(Eigen::VectorXd& original, Eigen::SparseMatrix<int>& wavelet, int rank, int numranks)
 {
 	int size = wavelet.rows();
-	std::vector<double> local_main = WaveletFlowForRank(original, wavelet, rank, numranks);
-	std::vector<double> total_main(size);
-	MPI_Reduce(&local_main[0], &total_main[0], size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-	return Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(total_main.data(), total_main.size());
+	std::vector<double> local_wavelet = WaveletFlowForRank(original, wavelet, rank, numranks);
+	std::vector<double> total_wavelet(size);
+	MPI_Reduce(&local_wavelet[0], &total_wavelet[0], size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Bcast(total_wavelet.data(), size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	return Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(total_wavelet.data(), total_wavelet.size());
 }
